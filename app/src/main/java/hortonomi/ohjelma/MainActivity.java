7bcc118891570eaf40f3a06311d666e2c9449023
PiaMarikainen <t9mapi00@students.oamk.fi>
2021-03-18 16:41:06 +0200

    18.3. Pia tehty 3 activity, sinne pääsee 2. activityn napista

--- app/src/main/java/hortonomi/ohjelma/MainActivity.java
+++ app/src/main/java/hortonomi/ohjelma/MainActivity.java
@@ -7,22 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button loginbutton = findViewById(R.id.loginbutton);
-        loginbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
+
+
+
+        loginbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+
+            public void onClick(View view)
+            {
                 System.out.println("Painiketta painettu");
 
                 Intent SecondIntent = new Intent(getApplicationContext(), Second.class);
                 startActivity(SecondIntent);
             }
         });
+
 
     }
 }
--- app/src/main/java/hortonomi/ohjelma/Second.java
+++ app/src/main/java/hortonomi/ohjelma/Second.java
@@ -2,7 +2,10 @@ package hortonomi.ohjelma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Second extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button buttonTesti3Activityyn = findViewById(R.id.buttonTesti3Activityyn);
+
+        buttonTesti3Activityyn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+
+            public void onClick(View view)
+            {
+                System.out.println("Painiketta testiActivity3 painettu");
+
+                Intent ThirdIntent = new Intent(getApplicationContext(), Third.class);
+                startActivity(ThirdIntent);
+            }
+        });
     }
 }
